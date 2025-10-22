55dc550c1cde1e4f577ef23f0b89a5dd458db134
Lynn Bou Francis <lynnboufrancis06@gmail.com>
2025-10-22 20:30:15 +0300

    ConnectFour.c
    Added the option to play against an easy bot

--- ConnectFour.c
+++ ConnectFour.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 char fourCheckers(char grid[6][7]){
     
@@ -81,10 +84,106 @@ void displayGrid(char arr[6][7]){
         printf("\n");
 }
 
+void inputWarning(char grid[6][7] , char player){
+        int placement;
+        int col;
+        //we keep scanning until the user enters a valid column
+        //do-while -> because we want it to be executed at leat once
+        do{
+            if (scanf("%d", &col) != 1) {
+                printf("Invalid input! Please enter a number.\n");
+ 
+            
+            // Clear the invalid input from the buffer
+            while (getchar() != '\n'); // discard characters until newline
+                continue; // skip the rest of the loop iteration 
+            }
+            
+            placement = placeChecker(grid, col, player);
+            if(placement==-1){
+                printf("Invalid column! Enter again\n");
+            }
+            else if(placement==0){
+                printf("Full column! Enter again\n");
+            }
+        }while(placement!=1);
+     
+}
+
+void Multiplayer(char* win, char grid[6][7], bool A){
+    char winner= *win;
+    //looping until a winner is found or grid is full
+
+    while(winner==' ' && !isGridFull(grid)){
+        char player = A ? 'A' : 'B'; 
+        printf("\nPlayer %c, choose a column (1-7):\n", player);
+        inputWarning(grid,player);
+        displayGrid(grid);
+        winner = fourCheckers(grid);
+        A = !A;// switch between the players
+    }
+    *win=winner;
+}
+
+void easyBot(char* win, char grid[6][7]) {
+    char winner = *win;
+
+    while (winner == ' ' && !isGridFull(grid)) {
+        printf("\nChoose a column (1–7): \n");
+        inputWarning(grid, 'A');
+        displayGrid(grid); // show grid right after player's move
+
+        winner = fourCheckers(grid);
+        if (winner != ' ' || isGridFull(grid)) break;
+
+        //  Bot's chooses a random column
+        srand(time(NULL));
+        while (true) {
+            int col = rand() % 7 + 1;
+            int check = placeChecker(grid, col, 'B');
+            if (check == 1) break;
+        }
+
+        printf("\nBot (B) played:\n");
+        displayGrid(grid);
+
+        winner = fourCheckers(grid);
+    }
+
+    *win = winner;
+}
+
 int main(){
     printf("Welcome to Connect Four!");
+
+    //printing the modes
+    printf("\nModes:");
+    printf("\n\tMultiplayer:1\n\tEasy:2\n\tMedium:3\n\tHard:4\n");
+
+    //user chooses the mode
+    printf("Enter your choice: "); 
+    int x;
+    do{
+        
+        if (scanf("%d",&x)!=1){
+            printf("Invalid input! Please enter a number.\n");
+
+            // Clear the invalid input from the buffer
+            while (getchar() != '\n'); // discard characters until newline
+                continue; // skip the rest of the loop iteration 
+            
+        }
+        if (x<1 || x>4){
+            printf("Please enter a valid number!\n");
+        }
+        else{
+            break;
+        }
+    }while(true);
+
     bool again = false;
     int pointsA = 0, pointsB = 0;
+
     do{
         //initialize the grid with empty characters
         char grid[6][7];
@@ -97,37 +196,16 @@ int main(){
         bool A = true; //boolean to determine whose turn is it
         char winner = ' '; 
 
-        //looping until a winner is found or grid is full
-        while(winner==' ' && !isGridFull(grid)){
-            char player = A ? 'A' : 'B';
-            int col;
-            printf("\nPlayer %c, choose a column (1-7): \n", player);
-            int placement;
-            //we keep scanning until the user enters a valid column
-            //do-while -> because we want it to be executed at leat once
-            do{
-                if (scanf("%d", &col) != 1) {
-                printf("Invalid input! Please enter a number.\n");
-                
-                // Clear the invalid input from the buffer
-                while (getchar() != '\n'); // discard characters until newline
-                }
-                
-                placement = placeChecker(grid, col, player);
-                if(placement==-1){
-                    printf("Invalid column! Enter again\n");
-                }
-                else if(placement==0){
-                    printf("Full column! Enter again\n");
-                }
-            }while(placement!=1);
+        char *win=&winner;  
 
-            //display grid
-            displayGrid(grid);
-
-            winner = fourCheckers(grid);
-            A = !A;
+        if(x==1){
+            Multiplayer(win,grid,A);
         }
+
+        else if(x==2){
+            easyBot(win,grid);
+        }
+        // other modes to be implemented in later sprints
 
         if(winner!=' '){
             printf("\n\033[1;32mPlayer %c wins! \033[0m\n", winner); //display winner
