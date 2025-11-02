e995c711ed68094723d4ea01dc51d188fce8d518
Lynn Bou Francis <lynnboufrancis06@gmail.com>
2025-11-02 12:34:24 +0200

    connect4.h

--- connect4.h
+++ connect4.h
@@ -12,5 +12,6 @@ int checkForMedium(char grid[6][7], int deltaRow, int deltaCol);
 int findPlayableCell(int startRow, int startCol, char grid[6][7], char target, int deltaRow, int deltaCol);
 void Multiplayer(char* win, char grid[6][7], bool A);
 void easyBot(char* win, char grid[6][7]);
+void mediumBot(char *win,char grid[6][7]);
 
 #endif
