4346e62e8c6e63e3da29e059ac1ad9827211b1d6
Roua-coder <rouanafeh6@gmail.com>
2025-11-02 10:59:46 +0200

    Update connect4.h

--- connect4.h
+++ connect4.h
@@ -1,1 +1,14 @@
+#ifndef CONNECT4_H
+#define CONNECT4_H
 
+#include <stdbool.h>
+
+char fourCheckers(char grid[6][7]);
+bool isGridFull(char grid[6][7]);
+int placeChecker(char grid[6][7], int col, char player);
+void displayGrid(char arr[6][7]);
+void inputWarning(char grid[6][7], char player);
+void Multiplayer(char* win, char grid[6][7], bool A);
+void easyBot(char* win, char grid[6][7]);
+
+#endif
