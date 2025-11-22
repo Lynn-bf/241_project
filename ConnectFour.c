b62afdb0ccb0341f1fc47dec74c18c7bf78c2115
Roua Nafeh <rouanafeh6@gmail.com>
2025-11-22 22:16:34 +0200

    Refactor Connect Four game logic and add AI

--- ConnectFour.c
+++ ConnectFour.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include <stdbool.h>
 #include "connect4.h"
+#define NEG_INF -100000
+#define POS_INF 100000
+#define max(a, b) ((a)>(b) ? (a) : (b))
+#define min(a, b) ((a)<(b) ? (a) : (b))
 
 char fourCheckers(char grid[6][7]){
     
@@ -253,16 +258,141 @@ void mediumBot(char *win,char grid[6][7]){
     *win = winner;
 }
 
+int evaluateBoard(char grid[6][7]){
+    int score = 0;
+    int directions[4][2] = {
+        {0, 1}, // horizontally
+        {1, 0}, //vertically
+        {1, 1}, // diagonal right 
+        {1, -1} //diagonal left
+    };
+
+    for(int row=0; row<6; row++){
+        if(grid[row][3]=='B') score+=5;
+        if(grid[row][3]=='A') score-=5;
+    }
+    for(int d = 0; d<4; d++){ //loop through all directions
+        int deltaRow = directions[d][0];
+        int deltaCol = directions[d][1];
+        for (int row = 0; row <6; row++) {   
+                
+            for (int col = 0; col <7; col++) {   
+
+                if(row+3*deltaRow<0 || row+3*deltaRow>=6 || col+3*deltaCol<0 || col+3*deltaCol>=7){
+                    continue;
+                }
+                char c1 = grid[row][col];
+                char c2 = grid[row+1*deltaRow][col+deltaCol*1];
+                char c3 = grid[row+2*deltaRow][col+deltaCol*2];
+                char c4 = grid[row+3*deltaRow][col+deltaCol*3];
+
+                int humanCount = (c1=='A') + (c2=='A') + (c3=='A') + (c4=='A');
+                int botCount = (c1=='B') + (c2=='B') + (c3=='B') + (c4=='B');
+                int emptyCount = (c1==' ') + (c2==' ') + (c3==' ') + (c4==' ');
 
 
+                if(botCount > 0 && humanCount > 0) continue;
+                
+                if(botCount==3 && emptyCount==1) score+=500;
+                if(botCount==2 && emptyCount==2) score+=10;
+                if(botCount==1 && emptyCount==3) score+=1;
 
+                if(humanCount==3 && emptyCount==1) score-=500;
+                if(humanCount==2 && emptyCount==2) score-=10;
+                if(humanCount==1 && emptyCount==3) score-=1;
+            }
+        }
+    }
+    return score;
+}
 
+int minimax(char grid[6][7], int depth, int alpha, int beta, bool isMaximizing){
+    //first step: check if this is the final solution
+    char winner = fourCheckers(grid); //bot has already won
+    if (winner=='A') return -10000; //human has already won
+    else if(winner=='B') return 10000;
+    else{ //no winner, 2 cases
+        if(isGridFull(grid)) return 0; //case 1: draw
+        else if(depth==0){ //case 2: can't go deeper
+            return evaluateBoard(grid); // 
+        }
+    }
 
+    //step 2: not terminal
+    if(isMaximizing){// Case 1: bot's turn 'B'
+        int bestScore = NEG_INF;
+        for(int col=0; col<=6; col++){
+            char gridcpy[6][7];
+            memcpy(gridcpy, grid, sizeof(char)*6*7); //copying the grid
+            int placed = placeChecker(gridcpy, col+1, 'B'); //placing 'B' in a different colum in each copy
+            if(placed==1){ //placement is successful
+                int score = minimax(gridcpy, depth-1, alpha, beta, false); //calling minimax recursively but for human's turn
+                bestScore = max(bestScore, score); //update bestScore
+                alpha = max(alpha, bestScore); //update alpha
+                if(beta<=alpha) break; //prune remaining columns
+            }
+        }
+        return bestScore;
+    } 
 
+    else{// Case 2: human's turn 'A'
+        int bestScore = POS_INF;
+        for(int col=0; col<=6; col++){
+            char gridcpy[6][7];
+            memcpy(gridcpy, grid, sizeof(char)*6*7);
+            int placed = placeChecker(gridcpy, col+1, 'A');
+            if(placed==1){
+                int score = minimax(gridcpy, depth-1, alpha, beta, true);
+                bestScore = min(bestScore, score);
+                beta = min(beta, bestScore);
+                if(beta<=alpha) break;
+            }
+        }
+        return bestScore;
+    }
+}
 
+void hardBot(char *win, char grid[6][7]){
+    char winner = *win;
+    
+    while(winner==' ' && !isGridFull(grid)){
+        winner = fourCheckers(grid);
+        if (winner != ' ') break;
 
+        printf("\nChoose a column (1-7): \n");
+        fflush(stdout);
+        inputWarning(grid, 'A');
+        displayGrid(grid);
 
+        winner = fourCheckers(grid);
+        if (winner != ' ') break;
 
+        int bestScore = NEG_INF;
+        int bestCol = -1;
+
+        for(int i=0; i<=6; i++){
+            char gridcpy[6][7];
+            memcpy(gridcpy, grid, sizeof(gridcpy));
+            int placed = placeChecker(gridcpy, i+1, 'B');
+            if(placed==1){
+                int score = minimax(gridcpy, 6, NEG_INF, POS_INF, false);
+                if(score>bestScore){
+                    bestScore = score;
+                    bestCol = i+1; //placeChecker expects columns between 1 and 7
+                }
+            }
+        }
+
+        if(bestCol==-1) return;
+
+        placeChecker(grid, bestCol, 'B');
+        printf("\nBot (B) played:\n");
+        displayGrid(grid);
+        winner = fourCheckers(grid);
+    }
+
+    *win = winner;
+}
 
 
 
